4963cb2767d3bd7d13599c725a6ce6e9e3c5afc1
newt239 <ysti2019.win2020.st@gmail.com>
2022-02-13 21:59:59 +0900

    update: message at home page

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,28 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
-import gsap from 'gsap'
-
-onMounted(() => {
-  const tl = gsap.timeline()
-  tl.to(".container", {
-    duration: 1,
-    opacity: 1
-  })
-})
+import { RouterView } from 'vue-router'
 </script>
 
-<template class="container">
+<template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <h1>NEWT HOUSE</h1>
   </header>
-
   <RouterView />
+  <div class="copyright">(c) 2022 newt</div>
 </template>
 
 <style lang="scss">
 #app {
   max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+  padding: 1rem;
 }
 
-.container {
-  opacity: 0;
+main {
+  margin: 1rem;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
+.copyright {
   width: 100%;
-  font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: $color-text;
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid black;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: $section-gap;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h2>WORKS</h2>
+    <h3>SHIP Notify</h3>
+    <p>
+      To notify the update of SHIP (
+      <a
+        href="https://ship.sakae-higashi.jp/"
+        target="_blank"
+      >https://ship.sakae-higashi.jp/</a> ) . Runnning on LINE ( as LINE Official Account ), Discord ( as bot at "SHIP Info" server ), Web, Twitter.
+    </p>
+    <h3>Quiz Flasher</h3>
+    <p>A tool to give a quiz. Import quiz data from Google Spreadsheet, then display these ranomly.</p>
+    <h3>Score Watcher</h3>
+    <p>When you hold the quiz games, you need to create scoreboard for player and watcher. Usually, scoreboard makes with Microsoft Excel, but it's difficult to reuse, so you need to make scoreboard from scratch. This software reduce these labor.</p>
+    <h2>CONTACT</h2>
+    <p>
+      GitHub:
+      <a href="https://github.com/newt239">@newt239</a>
+    </p>
   </main>
 </template>
