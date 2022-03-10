486e409ba214c4c035bdba1393b0b89416996b23
newt239 <ysti2019.win2020.st@gmail.com>
2022-03-10 22:27:23 +0900

    add: avatar to header and borderline at works

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,36 @@ import { RouterView } from 'vue-router'
 
 <template>
   <header>
-    <h1>NEWT HOUSE</h1>
+    <svg
+      viewBox="0 0 36 36"
+      fill="none"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+    >
+      <mask id="mask__beam" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36">
+        <rect width="36" height="36" rx="72" fill="#FFFFFF" />
+      </mask>
+      <g mask="url(#mask__beam)">
+        <rect width="36" height="36" fill="#ff9600" />
+        <rect
+          x="0"
+          y="0"
+          width="36"
+          height="36"
+          transform="translate(8 -4) rotate(198 18 18) scale(1)"
+          fill="#0099ff"
+          rx="6"
+        />
+        <g transform="translate(4 -1) rotate(-8 18 18)">
+          <path d="M15 19c2 1 4 1 6 0" stroke="#FFFFFF" fill="none" stroke-linecap="round" />
+          <rect x="11" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#FFFFFF" />
+          <rect x="23" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#FFFFFF" />
+        </g>
+      </g>
+    </svg>
+    <h1>newt</h1>
   </header>
   <RouterView />
   <div class="copyright">(c) 2022 newt</div>
@@ -15,6 +44,12 @@ import { RouterView } from 'vue-router'
   max-width: 1280px;
   margin: 0 auto;
   padding: 1rem;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
 }
 
 main {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,22 +4,51 @@
 <template>
   <main>
     <h2>WORKS</h2>
-    <h3>SHIP Notify</h3>
-    <p>
-      To notify the update of SHIP (
-      <a
-        href="https://ship.sakae-higashi.jp/"
-        target="_blank"
-      >https://ship.sakae-higashi.jp/</a> ) . Runnning on LINE ( as LINE Official Account ), Discord ( as bot at "SHIP Info" server ), Web, Twitter.
-    </p>
-    <h3>Quiz Flasher</h3>
-    <p>A tool to give a quiz. Import quiz data from Google Spreadsheet, then display these ranomly.</p>
-    <h3>Score Watcher</h3>
-    <p>When you hold the quiz games, you need to create scoreboard for player and watcher. Usually, scoreboard makes with Microsoft Excel, but it's difficult to reuse, so you need to make scoreboard from scratch. This software reduce these labor.</p>
+    <div class="cardGrid">
+      <div class="card ship-notify">
+        <h3>SHIP Notify</h3>
+        <p>
+          To notify the update of SHIP (
+          <a
+            href="https://ship.sakae-higashi.jp/"
+            target="_blank"
+          >https://ship.sakae-higashi.jp/</a> ) . Runnning on LINE ( as LINE Official Account ), Discord ( as bot at "SHIP Info" server ), Web, Twitter.
+        </p>
+      </div>
+      <div class="card quiz-flasher">
+        <h3>Quiz Flasher</h3>
+        <p>A tool to give a quiz. Import quiz data from Google Spreadsheet, then display these randomly.</p>
+      </div>
+      <div class="card">
+        <h3>Score Watcher</h3>
+        <p>When you hold the quiz games, you need to create scoreboard for player and watcher. Usually, scoreboard makes with Microsoft Excel, but it's difficult to reuse, so you need to make scoreboard from scratch. This software reduce these labor.</p>
+      </div>
+    </div>
     <h2>CONTACT</h2>
     <p>
       GitHub:
-      <a href="https://github.com/newt239">@newt239</a>
+      <a href="https://github.com/newt239" target="_blank">@newt239</a>
     </p>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.cardGrid {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  .card {
+    padding: 0 1rem;
+    border: 1px $color-text solid;
+    border-radius: 1rem;
+  }
+}
+.ship-notify {
+  grid-column: 1 2;
+  grid-row: 1 2;
+}
+.quiz-flasher {
+  grid-column: 2 3;
+  grid-row: 1 2;
+}
+</style>
