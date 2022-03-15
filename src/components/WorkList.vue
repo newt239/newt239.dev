fbe1a8ad4abcf38b6752c9129a8ca5470f14248e
newt239 <ysti2019.win2020.st@gmail.com>
2022-03-15 22:02:58 +0900

    add: thumbnails

---
+++ src/components/WorkList.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+type worksProp = {
+  id: string;
+  title: string;
+  thumbnail?: string;
+  description: string;
+}[]
+
+const works = ref<worksProp>([
+  {
+    id: "ship-notify",
+    title: "SHIP Notify",
+    thumbnail: "src/assets/ship-info-screenshot.webp",
+    description: "To notify the update of SHIP. Runnning on LINE ( as LINE Official Account ), Discord ( as bot at 'SHIP Info' server ), Web, Twitter."
+  },
+  {
+    id: "quiz-flasher",
+    title: "Quiz Flasher",
+    thumbnail: "src/assets/quiz-flasher-screenshot.webp",
+    description: "A tool to give a quiz. Import quiz data from Google Spreadsheet, then display these randomly."
+  },
+  {
+    id: "score-watcher",
+    title: "Score Watcher",
+    thumbnail: "src/assets/score-watcher_thumbnail.webp",
+    description: "When you hold the quiz games, you need to create scoreboard for player and watcher. Usually, scoreboard makes with Microsoft Excel, but it's difficult to reuse, so you need to make scoreboard from scratch. This software reduce these labor."
+  }
+]);
+</script>
+
+<template>
+  <div class="cardGrid">
+    <RouterLink v-for="work in works" :key="work.id" :to="'/works/' + work.id">
+      <div class="card">
+        <img :v-show="work.thumbnail" class="card-thumbnail" :src="work.thumbnail" />
+        <div class="card-body">
+          <h3>{{ work.title }}</h3>
+          <p>{{ work.description }}</p>
+        </div>
+      </div>
+    </RouterLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+a {
+  color: white;
+}
+.cardGrid {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  .card {
+    border: 1px $color-text solid;
+    border-radius: 1rem;
+    .card-thumbnail {
+      width: 100%;
+      border-radius: 1rem 1rem 0 0;
+    }
+    .card-body {
+      margin: 0 1rem;
+      h3 {
+        margin: 0;
+      }
+    }
+  }
+}
+.ship-notify {
+  grid-column: 1 2;
+  grid-row: 1 2;
+}
+.quiz-flasher {
+  grid-column: 2 3;
+  grid-row: 1 2;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,11 @@
 <script setup lang="ts">
+import WorkList from '@/components/WorkList.vue'
 </script>
 
 <template>
   <main>
     <h2>WORKS</h2>
-    <div class="cardGrid">
-      <div class="card ship-notify">
-        <div class="card-body">
-          <h3>SHIP Notify</h3>
-          <p>
-            To notify the update of SHIP (
-            <a
-              href="https://ship.sakae-higashi.jp/"
-              target="_blank"
-            >https://ship.sakae-higashi.jp/</a> ) . Runnning on LINE ( as LINE Official Account ), Discord ( as bot at "SHIP Info" server ), Web, Twitter.
-          </p>
-        </div>
-      </div>
-      <div class="card quiz-flasher">
-        <div class="card-body">
-          <h3>Quiz Flasher</h3>
-          <p>A tool to give a quiz. Import quiz data from Google Spreadsheet, then display these randomly.</p>
-        </div>
-      </div>
-      <div class="card score-watcher">
-        <img class="card-thumbnail" src="@/assets/score-watcher_thumbnail.webp" />
-        <div class="card-body">
-          <h3>Score Watcher</h3>
-          <p>When you hold the quiz games, you need to create scoreboard for player and watcher. Usually, scoreboard makes with Microsoft Excel, but it's difficult to reuse, so you need to make scoreboard from scratch. This software reduce these labor.</p>
-        </div>
-      </div>
-    </div>
+    <WorkList />
     <h2>CONTACT</h2>
     <p>
       GitHub:
@@ -40,28 +15,4 @@
 </template>
 
 <style lang="scss" scoped>
-.cardGrid {
-  display: grid;
-  gap: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  .card {
-    border: 1px $color-text solid;
-    border-radius: 1rem;
-    .card-thumbnail {
-      width: 100%;
-      border-radius: 1rem 1rem 0 0;
-    }
-    .card-body {
-      margin: 0 1rem;
-    }
-  }
-}
-.ship-notify {
-  grid-column: 1 2;
-  grid-row: 1 2;
-}
-.quiz-flasher {
-  grid-column: 2 3;
-  grid-row: 1 2;
-}
 </style>
