32b5947df5865965f05ec81aa83202b3f51a94d0
newt239 <ysti2019.win2020.st@gmail.com>
2022-03-16 20:20:23 +0900

    improve: align the height of the work cards

--- src/components/WorkList.vue
+++ src/components/WorkList.vue
@@ -72,6 +72,11 @@ a {
       }
       p {
         margin-top: 0;
+        height: 5rem;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 3;
+        overflow: hidden;
       }
     }
   }
