c638e5405bfa827248ccdf5d5849602fbfed5d4f
newt239 <ysti2019.win2020.st@gmail.com>
2022-03-17 20:45:42 +0900

    feature: add gtag

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 
 app.use(router)
+
+
+app.use(VueGtag, {
+    config: { id: "G-WP0FNML0VP" }
+})
 
 app.mount('#app')
