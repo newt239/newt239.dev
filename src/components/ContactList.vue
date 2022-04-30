a63ae9b8b62d7d80936732fe4be650b3ba2e3d73
newt239 <ysti2019.win2020.st@gmail.com>
2022-04-30 21:39:07 +0900

    change: unify indent 4 to 2

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -2,65 +2,65 @@
 import { ref } from 'vue';
 
 type contactsProp = {
-    name: string;
-    icon: string;
-    id: string;
-    url: string;
-    color?: string;
+  name: string;
+  icon: string;
+  id: string;
+  url: string;
+  color?: string;
 }[]
 
 const contacts = ref<contactsProp>([
-    {
-        name: "GitHub",
-        icon: "/img/github.svg",
-        id: "@newt239",
-        url: "https://github.com/newt239",
-        color: "#FFF"
-    }, {
-        name: "QiIta",
-        icon: "/img/qiita.png",
-        id: "@newt239",
-        url: "https://qiita.com/newt239",
-        color: "#55C500"
-    }
+  {
+    name: "GitHub",
+    icon: "/img/github.svg",
+    id: "@newt239",
+    url: "https://github.com/newt239",
+    color: "#FFF"
+  }, {
+    name: "QiIta",
+    icon: "/img/qiita.png",
+    id: "@newt239",
+    url: "https://qiita.com/newt239",
+    color: "#55C500"
+  }
 ]);
 </script>
 
 <template>
-    <div class="contacts">
-        <a class="contact" :style="'border: 1px ' + contact.color + ' solid;color: ' + contact.color"
-            v-for="contact in contacts" :key="contact.name" :href="contact.url" :alt="contact.name" target="_blank">
-            <div>
-                <img :src="contact.icon" />
-                <span>{{ contact.id }}</span>
-            </div>
-        </a>
-    </div>
+  <div class="contacts">
+    <a class="contact" :style="'border: 1px ' + contact.color + ' solid;color: ' + contact.color"
+      v-for="contact in contacts" :key="contact.name" :href="contact.url" :alt="contact.name" target="_blank">
+      <div>
+        <img :src="contact.icon" />
+        <span>{{ contact.id }}</span>
+      </div>
+    </a>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .contacts {
-    display: flex;
-    gap: 1rem;
+  display: flex;
+  gap: 1rem;
 
-    .contact {
-        padding: 1rem;
-        border-radius: 1rem;
-        transition: all 0.5s;
+  .contact {
+    padding: 1rem;
+    border-radius: 1rem;
+    transition: all 0.5s;
 
-        div {
-            display: flex;
-            align-items: center;
-            gap: 1rem;
+    div {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
 
-            img {
-                height: 1.5rem;
-            }
-        }
+      img {
+        height: 1.5rem;
+      }
     }
+  }
 
-    @include mq(mobile) {
-        flex-direction: column;
-    }
+  @include mq(mobile) {
+    flex-direction: column;
+  }
 }
 </style>
--- src/views/WorksView.vue
+++ src/views/WorksView.vue
@@ -3,155 +3,155 @@ import { RouterView, RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <main>
-        <h2>WORKS</h2>
-        <div class="work">
-            <RouterView />
-        </div>
-        <div class="footer">
-            <RouterLink class="back" to="/">BACK HOME</RouterLink>
-        </div>
-    </main>
+  <main>
+    <h2>WORKS</h2>
+    <div class="work">
+      <RouterView />
+    </div>
+    <div class="footer">
+      <RouterLink class="back" to="/">BACK HOME</RouterLink>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
 .work {
-    border: 1px $color-white solid;
-    border-radius: 1rem;
+  border: 1px $color-white solid;
+  border-radius: 1rem;
 
-    .about {
-        display: flex;
-        justify-content: space-between;
+  .about {
+    display: flex;
+    justify-content: space-between;
 
-        .intro {
-            .summary {
-                table {
-                    border-spacing: 0 0.5rem;
-
-                    th,
-                    td {
-                        padding: 0 1rem;
-                    }
-
-                    th {
-                        border-right: 1px $color-white solid;
-                    }
-                }
-            }
-        }
-
-        .thumbnail {
-            width: 50%;
-
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-                border-radius: 0 1rem 0 0;
-            }
-        }
-
-        @include mq(tablet) {
-            flex-direction: column-reverse;
-
-            .thumbnail {
-                width: 100%;
-
-                img {
-                    border-radius: 1rem 1rem 0 0;
-                }
-            }
-
-            .intro h3 {
-                margin: 0;
-            }
-        }
-    }
-
-    .content {
-        padding-bottom: 1rem;
-
-        p,
-        ul {
-            margin: 1rem;
-        }
-
-        h4 {
-            display: inline-block;
-            padding: 0 1rem;
-            margin-bottom: 0;
-            font-size: 1.5rem;
-            border-bottom: $color-white 1px solid;
-        }
-
-        h5 {
-            font-size: 1.3rem;
-            margin: 1rem 1rem 0;
-        }
-
-        h6 {
-            margin: 0 1rem;
-            font-size: 1rem;
-        }
-
+    .intro {
+      .summary {
         table {
-            margin: 0 1rem;
-            border-spacing: 0 0.5rem;
+          border-spacing: 0 0.5rem;
 
-            th,
-            td {
-                padding: 0 1rem;
-            }
+          th,
+          td {
+            padding: 0 1rem;
+          }
 
-            th {
-                border-right: 1px $color-white solid;
-            }
+          th {
+            border-right: 1px $color-white solid;
+          }
         }
-
-        code,
-        pre {
-            padding: 0 0.3rem;
-            border-radius: 0.5rem;
-            margin: 0 0.2rem;
-            background-color: $color-black-secondary;
-        }
-
-        pre {
-            overflow-x: scroll;
-        }
-
-        .imageWrapper {
-            text-align: center;
-
-            img {
-                max-width: calc(100% - 1rem);
-                max-height: 50vh;
-                filter: drop-shadow(2px 4px 6px black);
-                border-radius: 0.5rem;
-            }
-        }
-
-        .url {
-            word-break: break-all;
-        }
+      }
     }
+
+    .thumbnail {
+      width: 50%;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 0 1rem 0 0;
+      }
+    }
+
+    @include mq(tablet) {
+      flex-direction: column-reverse;
+
+      .thumbnail {
+        width: 100%;
+
+        img {
+          border-radius: 1rem 1rem 0 0;
+        }
+      }
+
+      .intro h3 {
+        margin: 0;
+      }
+    }
+  }
+
+  .content {
+    padding-bottom: 1rem;
+
+    p,
+    ul {
+      margin: 1rem;
+    }
+
+    h4 {
+      display: inline-block;
+      padding: 0 1rem;
+      margin-bottom: 0;
+      font-size: 1.5rem;
+      border-bottom: $color-white 1px solid;
+    }
+
+    h5 {
+      font-size: 1.3rem;
+      margin: 1rem 1rem 0;
+    }
+
+    h6 {
+      margin: 0 1rem;
+      font-size: 1rem;
+    }
+
+    table {
+      margin: 0 1rem;
+      border-spacing: 0 0.5rem;
+
+      th,
+      td {
+        padding: 0 1rem;
+      }
+
+      th {
+        border-right: 1px $color-white solid;
+      }
+    }
+
+    code,
+    pre {
+      padding: 0 0.3rem;
+      border-radius: 0.5rem;
+      margin: 0 0.2rem;
+      background-color: $color-black-secondary;
+    }
+
+    pre {
+      overflow-x: scroll;
+    }
+
+    .imageWrapper {
+      text-align: center;
+
+      img {
+        max-width: calc(100% - 1rem);
+        max-height: 50vh;
+        filter: drop-shadow(2px 4px 6px black);
+        border-radius: 0.5rem;
+      }
+    }
+
+    .url {
+      word-break: break-all;
+    }
+  }
 }
 
 .footer {
-    padding: 1rem;
-    text-align: center;
+  padding: 1rem;
+  text-align: center;
 
-    .back {
-        display: inline-block;
-        padding: 0.5rem 1rem;
-        border-radius: 0.5rem;
-        color: $color-white;
+  .back {
+    display: inline-block;
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    color: $color-white;
 
-        &::before {
-            padding-right: 5px;
-            content: url('data:image/svg+xml;utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>');
-            vertical-align: -0.1rem;
-        }
+    &::before {
+      padding-right: 5px;
+      content: url('data:image/svg+xml;utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>');
+      vertical-align: -0.1rem;
     }
+  }
 }
 </style>
