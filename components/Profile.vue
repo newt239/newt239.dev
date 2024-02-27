af4e6f07fef72514b369e145bc8c4874bddf4f20
newt239 <ysti2019.win2020.st@gmail.com>
2024-02-27 11:56:11 +0900

    update: profile

--- components/Profile.vue
+++ components/Profile.vue
@@ -5,8 +5,9 @@ const age = dayjs().diff(dayjs("2005-11-02"), "year");
 
 const profile: string[] = [
   `${age}さい`,
-  "Webフロントエンドの開発をしています",
-  "UI / UXに興味があります",
+  "すきなもの: Webフロントエンド, UI / UX, アクセシビリティ",
+  "言語: TypeScript, Python, Goなど",
+  "趣味: アニメ鑑賞, ボカロ",
 ];
 </script>
 
@@ -25,24 +26,23 @@ const profile: string[] = [
 
 <style>
 .top-card {
-  background-image: linear-gradient(
-    to bottom right,
-    oklch(80% 0.4 222),
-    oklch(35% 0.5 313)
-  );
+  background-image: linear-gradient(to bottom right,
+      oklch(80% 0.4 222),
+      oklch(35% 0.5 313));
   border-radius: 0.5rem;
   padding: 5vh 5vw 10vh;
   filter: drop-shadow(2px 4px 6px black);
+
   @media screen and (max-width: 768px) {
-    background-image: linear-gradient(
-      0deg,
-      rgba(69, 69, 214, 1) 0%,
-      rgba(0, 212, 255, 1) 100%
-    );
+    background-image: linear-gradient(0deg,
+        rgba(69, 69, 214, 1) 0%,
+        rgba(0, 212, 255, 1) 100%);
   }
 }
+
 .profile {
   position: relative;
+
   ul {
     line-height: 2;
     margin: 0;
