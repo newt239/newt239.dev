f71a979c1abe9665d69627bbcad2230d7830cb23
newt239 <ysti2019.win2020.st@gmail.com>
2024-05-09 17:15:40 +0900

    improve: update about page

--- pages/about.vue
+++ pages/about.vue
@@ -6,8 +6,9 @@ useHead({
 
 <template>
   <main>
-    <div class="container">
-      <div class="about-me">
+    <div class="container aboutPage">
+      <div class="aboutMeSpace"></div>
+      <div class="aboutMe">
         <img src="/icon.webp" alt="プロフィール画像" class="profile-icon" />
         <div class="short-description">
           <h2>newt <span class="sub">@newt239</span></h2>
@@ -20,6 +21,30 @@ useHead({
         </div>
       </div>
       <div class="profile-detail">
+        <h3>コンタクト</h3>
+        <ul>
+          <li>
+            メール
+            <ul>
+              <li><a href="mailto:contact@newt239.dev">contact@newt239.dev</a></li>
+            </ul>
+          </li>
+          <li>
+            Discordサーバー
+            <ul>
+              <li>
+                <a href="https://discord.gg/rct5sx6rbZ" target="_blank">https://discord.gg/rct5sx6rbZ</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <h3>興味・関心</h3>
+        <ul>
+          <li>Webフロントエンド</li>
+          <li>UI/UX, ユーザビリティ</li>
+          <li>アクセシビリティ</li>
+          <li>HCI</li>
+        </ul>
         <h3>開発</h3>
         <h4>言語や技術</h4>
         <ul>
@@ -30,6 +55,7 @@ useHead({
             <li>Emotion, Panda CSS, Kuma UI, Tailwind</li>
             <li>jotai, Recoil</li>
           </ul>
+          <li>CSS</li>
           <li>Python</li>
           <ul>
             <li>Selenium, BeautifulSoup, pycord</li>
@@ -48,7 +74,7 @@ useHead({
           </ul>
           <li>LaTeX</li>
         </ul>
-        <h4>環境</h4>
+        <h4>開発環境</h4>
         <ul>
           <li>MacBook Air M3</li>
           <ul>
@@ -73,22 +99,12 @@ useHead({
           <li>HoneyWorks</li>
           <li>Mrs. GREEN APPLE</li>
         </ul>
-        <h3>コンタクト</h3>
+        <h4>好きな書体</h4>
         <ul>
-          <li>
-            メール
-            <ul>
-              <li><a href="mailto:contact@newt239.dev">contact@newt239.dev</a></li>
-            </ul>
-          </li>
-          <li>
-            Discordサーバー
-            <ul>
-              <li>
-                <a href="https://discord.gg/rct5sx6rbZ" target="_blank">https://discord.gg/rct5sx6rbZ</a>
-              </li>
-            </ul>
-          </li>
+          <li>LINE Seed JP</li>
+          <li>ユールカ</li>
+          <li>新ゴ</li>
+          <li>筑紫アンティークゴシック</li>
         </ul>
         <h3>リンク集</h3>
         <ul>
@@ -101,61 +117,123 @@ useHead({
 </template>
 
 <style>
-.sub {
-  font-size: 50%;
-  color: rgb(var(--color-text-secondary));
-}
-
-.about-me {
+.aboutPage {
+  position: relative;
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  max-width: 550px;
-  gap: 2rem;
-  margin: 0 auto;
+  flex-wrap: wrap;
 
-  @media screen and (max-width: 700px) {
+  @media screen and (max-width: 1080px) {
     flex-direction: column;
+  }
 
-    .short-description {
-      width: 100%;
+  .aboutMeSpace {
+    width: 50%;
+    height: 100vh;
+
+    @media screen and (max-width: 1080px) {
+      display: none;
     }
   }
 
-  h2 {
-    padding: 0 0 0.5rem;
+  .aboutMe {
+    position: fixed;
+    padding-right: 2rem;
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%) translateX(-100%);
+    width: calc(50% + 2rem);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 550px;
+    gap: 2rem;
+
+    @media screen and (max-width: 1080px) {
+      flex-direction: row;
+      position: relative;
+      transform: none;
+      top: 0;
+      left: 0;
+      width: 100%;
+      flex-direction: column;
+      margin: 0 auto;
+
+      .short-description {
+        width: 100%;
+      }
+    }
+
+    @media screen and (max-width: 700px) {
+      flex-direction: column;
+
+      .short-description {
+        width: 100%;
+      }
+    }
+
+    h2 {
+      padding: 0 0 0.5rem;
+    }
+
+    h3,
+    h4 {
+      padding-left: 0;
+    }
   }
 
-  h3,
-  h4 {
-    padding-left: 0;
+  .profile-icon {
+    width: 80%;
+    max-width: 250px;
+    height: auto;
+    border-radius: 50%;
+    aspect-ratio: 1 / 1;
+    display: block;
   }
-}
 
-.profile-icon {
-  width: 80%;
-  max-width: 250px;
-  height: auto;
-  border-radius: 50%;
-  aspect-ratio: 1 / 1;
-  display: block;
-}
+  .profile-detail {
+    margin: 0 auto;
+    width: 50%;
+    max-width: 600px;
+    padding-top: 2rem;
+    margin-top: 2rem;
 
-.profile-detail {
-  margin: 0 auto;
-  width: 100%;
-  max-width: 600px;
-  border-width: 1px 0 0 0;
-  border-style: solid;
-  border-color: rgb(var(--color-text-secondary));
-  padding-top: 2rem;
-  margin-top: 2rem;
+    @media screen and (max-width: 1080px) {
+      width: 100%;
+      max-width: 1080px;
+      ;
+      margin: 0;
+    }
 
-  h3,
-  h4 {
-    padding-left: 0;
+    h3 {
+      position: relative;
+      display: inline-block;
+      width: 100%;
+      text-align: center;
+    }
+
+    h3:before,
+    h3:after {
+      content: '';
+      position: absolute;
+      top: 60%;
+      display: inline-block;
+      width: calc(50% - 4rem);
+      height: 1px;
+      background-color: rgb(var(--color-text-secondary));
+    }
+
+    h3:before {
+      left: 0;
+    }
+
+    h3:after {
+      right: 0;
+    }
+
+    h4 {
+      padding-left: 0;
+    }
   }
 }
 </style>
