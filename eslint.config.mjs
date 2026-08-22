2abc14bfcad9066b7c6738f0fe91941c10dfd94b
newt <ysti2019.win2020.st@gmail.com>
2026-08-22 15:56:23 +0900

    chore: vue/html-self-closing を無効化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,5 +9,6 @@ export default withNuxt(
     rules: {
       "vue/multi-word-component-names": "off",
       "vue/first-attribute-linebreak": "off",
+      "vue/html-self-closing": "off",
     }
   });
